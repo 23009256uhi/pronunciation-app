49d4e498346a63c0d757b35c9d4e4fba1b417811
23009256uhi <23009256uhi@gmail.com>
2024-11-10 12:16:23 +0000

    server code cleanup

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ import dotenv from "dotenv";
 import axios from "axios";
 import { metaphone } from "metaphone";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { db } from "./firebaseConfig.js";
-import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const app = express();
 const PORT = 5000;
@@ -17,19 +15,18 @@ dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const genAI = new GoogleGenerativeAI("AIzaSyBrmDL_aTUD9mYweK1wehLyJ6wx8sbB0aQ");
+// Google API key
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+// Gemini 1.5 model
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+// Function to run prompt
 async function run(prompt) {
   try {
     const result = await model.generateContent(prompt);
     const response = await result.response;
     const text = response.text();
-    // console.log(text);
     return text;
   } catch (error) {
     console.error("Error generating AI response:", error);
@@ -41,7 +38,6 @@ let history = [];
 let messages = [{ role: "ai", text: "Hi! What do you want to talk about?" }];
 
 async function chat(message) {
-  // const chat = model.startChat(chatId);
   const chat = model.startChat({
     history: history,
     generationConfig: {
@@ -52,12 +48,8 @@ async function chat(message) {
   const result = await chat.sendMessage(message);
   const response = await result.response;
   const text = response.text();
-  // console.log(text);
   return text;
 }
-
-// Write a function to give words to practice the wrong sound
-// Implement text to speech
 
 // Endpoint for fetching word data from dictionary API
 app.post("/fetchword", async (req, res) => {
@@ -86,14 +78,9 @@ app.post("/fetchword", async (req, res) => {
 app.post("/word", async (req, res) => {
   const { userInput } = req.body;
 
-  // if ((userInput.length = 0)) {
-  //   return res.status(400).json({ error: "Input required" });
-  // }
-
   try {
     const prompt = `If '${userInput}' is an English word, could you please reply with the phonetic transliteration of the word?`;
     const reply = await run(prompt);
-    console.log(reply);
     return res.json(reply);
   } catch (error) {
     console.error("Error:", error);
@@ -119,14 +106,10 @@ app.post("/analyse", async (req, res) => {
     const expectedCode = metaphone(expectedWord);
 
     if (recognizedCode === expectedCode) {
-      // return res.json(true);
-      // const prompt = `The user said "${recognizedText}" but should have said "${expectedWord}". Provide detailed feedback on how to improve their pronunciation, focusing on the differences between "${recognizedCode}" and "${expectedCode}". Just say 1. what is wrong and 2. how to improve. Reply as it was a conversation between you and the user and don't make a list`;
-      // const reply = await run(prompt);
       return res.json("Correct!");
     } else {
       const prompt = `The user said "${recognizedText}" but should have said "${expectedWord}". Provide detailed feedback on how to improve their pronunciation, focusing on the differences between "${recognizedCode}" and "${expectedCode}". Just say 1. what is wrong and 2. how to improve. Reply as it was a conversation between you and the user and don't make a list`;
       const reply = await run(prompt);
-      // return res.json(false);
       return res.json(reply);
     }
   } catch (error) {
@@ -171,7 +154,6 @@ app.post("/conversation", async (req, res) => {
     console.log("reply:", reply);
     console.log(history[0]);
     return res.json(reply);
-    // const conversationRef = db.collection("conversations").doc(conversationId);
   } catch (error) {
     console.error("Error handling conversation:", error);
   }
